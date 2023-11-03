b3e73d3736feca3b8f5ce1d0f9ae893bac29ef42
Bilel Jemai <Bileljemai97@gmail.com>
2023-11-03 23:59:03 +0100

    Service

--- src/app/shared/Service/Reglement.service.ts
+++ src/app/shared/Service/Reglement.service.ts
@@ -6,7 +6,7 @@ import {HttpClient} from '@angular/common/http';
   providedIn: 'root'
 })
 export class ReglementService {
-  readonly API_URL = 'http://192.168.1.222/SpringMVC/reglement';
+  readonly API_URL = 'http://192.168.1.222:7070/SpringMVC/reglement';
 
   constructor(private httpClient: HttpClient) {
   }
